ca619201c035ddd4424ac46af3611dbe82ae944f
lemon <lemon>
2019-05-29 16:54:48 -0400

    1.[ADD] backend management shared layout
    2.[ADD] display father module list

---
+++ ClairG.GamingForum/admin/inc/header.inc.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>adminstrator - backend management</title>
+        <meta name="keywords" content="backend management" />
+        <meta name="description" content="backend management" />
+        <link rel="stylesheet" type="text/css" href="style/public.css" />
+    </head>
+    <body>
+    	<!-- top bar -->
+    	<div id="top">
+		<div class="logo">
+			Administrator Center
+		</div>
+		<div class="login_info">
+			<a href="#" style="color:#fff;">Home Page</a>&nbsp;|&nbsp;
+			Administrator: admin <a href="#">[logout]</a>
+		</div>
+	</div>
+    	<!-- side bar -->
+    	<div id="sidebar">
+		<ul>
+			<li>
+				<div class="small_title">System</div>
+				<ul class="child">
+					<li><a class="current" href="#">System Information</a></li>
+					<li><a href="#">Administrator</a></li>
+					<li><a href="#">Add Administrator</a></li>
+					<li><a href="#">Setting</a></li>
+				</ul>
+			</li>
+			<li>
+				<div class="small_title">Content Management</div>
+				<ul class="child">
+					<li><a class="current" href="#">Father Module List</a></li>
+					<li><a href="#">Add Father Module</a></li>
+					<li><a href="#">Child Module List</a></li>
+					<li><a href="#">Add Child Module</a></li>
+					<li><a href="#">Thread Management</a></li>
+				</ul>
+			</li>
+			<li>
+				<div class="small_title">User Management</div>
+				<ul class="child">
+					<li><a href="#">User List</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>    	
